142c20dd9ad01b08f907ca5b7c2b97dbe2f713fe
seannives <seann.ives@gmail.com>
2013-11-21 17:44:01 -0500

    if you're correct on your last attempt you shouldn't have the correct answer returned

--- test/integration/ce-integration.tests.js
+++ test/integration/ce-integration.tests.js
@@ -26,6 +26,7 @@ var Controller = require('../../lib/controller');
 var utils = require('../../lib/utils');
 
 var multiplechoice_incorrect_last = require('../test_messages/multiplechoice_incorrect_last.json');
+var multiplechoice_incorrect_notlast = require('../test_messages/multiplechoice_incorrect_notlast.json');
 
 
 describe('IPC -> CE Multiple Choice', function()
@@ -40,7 +41,36 @@ describe('IPC -> CE Multiple Choice', function()
         done();
     });
 
-    it('should return a valid Result given a valid but incorrect submission message', function (done) {
+    it('should return a valid Result given a valid but incorrect submission message on not last attempt', function (done) {
+        var properReturnVal = {
+            "correctness": 0,
+            "feedback": "Does the fertility rate change with population size?",
+            "correctAnswer": null
+        };
+        var message = utils.cloneObject(multiplechoice_incorrect_notlast);
+        request(server.listener)
+            .post('/assessments')
+            .send(message)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/) // Verify the content type
+            .expect(200) // Verify the result code (200=OK)
+            .end(function(err, result)
+            {
+                if (err) return done(err);
+                try {
+                    //console.log("@@@:"+JSON.stringify(result.body));
+                    expect(result.body.code).to.equal(200);
+                    expect(result.body.status).to.equal('success');
+                    expect(result.body.data).to.deep.equal(properReturnVal);
+                    done();
+                } catch (e) {
+                    done(e);
+                }
+            }
+        );
+    });
+
+    it('should return a valid Result given a valid but incorrect submission message on last attempt', function (done) {
 		var properReturnVal = {
             "correctness": 0,
             "feedback": "This might happen but is it something is necessarily occurs?",
@@ -102,10 +132,7 @@ describe('IPC -> CE Multiple Choice', function()
 		var properReturnVal = {
             "correctness": 1,
             "feedback": "Your answer <%= studAnsValue %> is correct. Growth rate stays constant.",
-            "correctAnswer": {
-                "key": "option000",
-                "feedback": "Your answer <%= studAnsValue %> is correct. Growth rate stays constant."
-            }
+            "correctAnswer": null
         };
 		var message = utils.cloneObject(multiplechoice_incorrect_last);
 		message.studentSubmission.submission = "option000";
