08ceb91da00a696a4edac46caeb4ad01e598ae67
seannives <seann.ives@gmail.com>
2014-12-03 13:56:42 -0500

    cleaning up programmingexercise.  adding integration tests

--- test/integration/programmingexercise_integration.tests.js
+++ test/integration/programmingexercise_integration.tests.js
@@ -1,14 +1,15 @@
 /* **************************************************************************
- * $Workfile:: programmingexercise.tests.js                                 $
+ * $Workfile:: programmingexercise_integration.tests.js                     $
  * *********************************************************************/ /**
  *
- * @fileoverview Contains unit tests for multiplechioce.js
+ * @fileoverview Contains integration tests for programmingexercise.js
+ * which hit the TC servers.
  *
  *
- * Created on       Sept 15, 2014
+ * Created on       Dec 3, 2014
  * @author          Seann Ives
  *
- * @copyright (c) 2013 Pearson, All rights reserved.
+ * @copyright (c) 2014 Pearson, All rights reserved.
  *
  * **************************************************************************/
 //force test environment
@@ -55,10 +56,6 @@ var mockdata2 = {
     "isLastAttempt": false
 };
 
-// @todo - A bunch of tests below are skipped and copied into 
-// programmingexercise_integration.tests.js.  It'd be great to mock them
-// and un-skip them here.
-
 /**
  * ProgrammingExercise Assessment tests.  We have to test these through the 
  * engine's assess method.
@@ -71,50 +68,11 @@ describe('ProgrammingExercise assessments', function() {
         ce = new CE.EngineHandler();
     });
 
-    it('should complain if answer is badly formatted', function (done) {
-        var data = utils.cloneObject(mockdata);
-        data.answerKey = {assessmentWrong: "thingy", answers: "string"};
-        ce.processSubmission(data, function(err, result)  {
-            try {
-                expect(err).to.not.be.null;
-                //expect(err.message).to.equal('Validation failed');
-                expect(result).to.be.null;
-                done();
-            }
-            catch (e)
-            {
-                done(e);
-            }
-        });
-    });
-
-    it('should complain if submission is badly formatted', function (done) {
-        var data = utils.cloneObject(mockdata);
-        data.studentSubmission = {"submissiony": {"thing": "so wrong"}};
-        ce.processSubmission(data, function(err, result)  {
-            try {
-                expect(err).to.not.be.null;
-                //console.log(err);
-                //expect(err.message).to.equal('Validation failed');
-                expect(result).to.be.null;
-                done();
-            }
-            catch (e)
-            {
-                done(e);
-            }
-        });
-    });
-
-//@todo - this works but we should do this with a mock.  You could put this as-is or close to that
-//        in the integration tests.
-    it.skip('should handle incorrect submission', function (done) {
+    it('should handle incorrect submission', function (done) {
         var data = utils.cloneObject(mockdata2);
         ce.processSubmission(data, function(err, result)  {
             try {
                 expect(result.correctness).to.equal(0);
-                //console.log(result);
-                //expect(result.feedback).to.equal('This might happen but is it something is necessarily occurs?');
                 expect(result.feedback).to.be.null;
                 expect(result.brixState.codeEvaluation).to.be.an('object');
                 expect(result.stats.response).to.not.be.null;
@@ -129,7 +87,7 @@ describe('ProgrammingExercise assessments', function() {
         });
     });
 
-    it.skip('should handle correct submission', function (done) {
+    it('should handle correct submission', function (done) {
         var data = utils.cloneObject(mockdata2);
         data.studentSubmission.entry = sub2;
         ce.processSubmission(data, function(err, result)  {
@@ -137,9 +95,6 @@ describe('ProgrammingExercise assessments', function() {
                 expect(result.correctness).to.equal(1);
                 expect(result.feedback).to.be.null;
                 expect(result.brixState.codeEvaluation).to.be.an('object');
-                //expect(result.stats.response).to.be.null;
-                //@todo - fix this
-                //expect(result.stats.answerId).to.equal('option000');
                 expect(result.stats.assessmentItemQuestionType).to.equal('ProgrammingExercise');
                 done();
             }
@@ -150,18 +105,16 @@ describe('ProgrammingExercise assessments', function() {
         });
     });
 
+    // @todo - this is bs copied from mcq
     it.skip('should report back the correct answer if isLastAttempt is true', function (done) {
         var data = utils.cloneObject(mockdata);
-        // set an incorrect answer, just for fun.
-        data.studentSubmission.key = "option003";
+        
         ce.processSubmission(data, function(err, result)  {
             try {
+                console.log(JSON.stringify(result));
+                console.log(result);
                 expect(result.correctness).to.equal(0);
-                expect(result.feedback).to.equal('This might happen but is it something is necessarily occurs?');
-                expect(result.correctAnswer.key).to.equal('option000');
-                expect(result.correctAnswer.feedback).to.equal('Your answer <%= studAnsValue %> is correct. Growth rate stays constant.');
                 expect(result.stats.response).to.be.null;
-                expect(result.stats.answerId).to.equal('option003');
                 expect(result.stats.assessmentItemQuestionType).to.equal('ProgrammingExercise');
                 done();
             }
@@ -172,6 +125,7 @@ describe('ProgrammingExercise assessments', function() {
         });
     });
 
+    // @todo - this is bs copied from mcq
     it.skip('should report back the correct answer with empty feedback string if isLastAttempt is true', function (done) {
         var data = utils.cloneObject(mockdata);
         // set an incorrect answer, just for fun.
@@ -198,6 +152,7 @@ describe('ProgrammingExercise assessments', function() {
         });
     });
 
+    // @todo - this is bs copied from mcq
     it.skip('should not report back the correct answer if isLastAttempt is false', function (done) {
         var data = utils.cloneObject(mockdata);
 
@@ -220,100 +175,4 @@ describe('ProgrammingExercise assessments', function() {
         });
     });
 
-});
-
-/**
- * ProgrammingExercise Assessment Retreive answer tests.  We have to test these through the 
- * engine's retrieveAnswer method.
- */
-describe('ProgrammingExercise retrieve answer', function() {
-    var ce = null;
-    var handler = null;
-
-    before(function () {
-        ce = new CE.EngineHandler();
-    });    
-
-    it('should return null if there is no correct answer', function(done) {
-        var data = utils.cloneObject(mockdata);
-        var answerKey = data.answerKey;
-
-        ce.retrieveAnswer(answerKey, function(err, result) {
-            try {
-                expect(result.correctAnswer).to.be.null;
-                done();
-            }
-            catch (e)
-            {
-                done(e);
-            }
-        });
-    });
-
-    it('should return an empty array if that is what is in the config', function(done) {
-        var data = utils.cloneObject(mockdata);
-        var answerKey = data.answerKey;
-        var answers = answerKey.answers;
-
-        var codeExamples = [];
-        _.extend(answerKey.answers,
-            {
-                "codeExamples": codeExamples
-            });
-
-        ce.retrieveAnswer(answerKey, function(err, result) {
-            try {
-                expect(result.correctAnswer.codeExamples).to.deep.equal(codeExamples);
-                done();
-            }
-            catch (e)
-            {
-                done(e);
-            }
-        });
-    });
-
-    it('should return all the codeExamples', function(done) {
-        var data = utils.cloneObject(mockdata);
-        var answerKey = data.answerKey;
-        var answers = answerKey.answers;
-
-        var codeArray1 = [
-            "for (total = 0.0, k = 0; k < n; k++) { \u00a0\u00a0\u00a0\u00a0total += temps[k]; avgTemp = total / n;} "
-        ];
-        var codeArray2 = [
-                            "for (total = 0.0, k = 0; k < n; k++)",
-                            "{",
-                            "\u00a0\u00a0\u00a0\u00a0total += temps[k];",
-                            "}",
-                            "",
-                            "avgTemp = total / n;"
-                        ];
-
-        _.extend(answerKey.answers,
-            {
-                "codeExamples":
-                [
-                    {
-                        "code": codeArray1
-                    },
-                    {
-                        "code": codeArray2
-                    }
-                ]
-            });
-
-        ce.retrieveAnswer(answerKey, function(err, result) {
-            try {
-                expect(result.correctAnswer.codeExamples[0].code).to.deep.equal(codeArray1);
-                expect(result.correctAnswer.codeExamples[1].code).to.deep.equal(codeArray2);
-                done();
-            }
-            catch (e)
-            {
-                done(e);
-            }
-        });
-    });   
-       
 });
